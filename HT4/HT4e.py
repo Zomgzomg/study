6444870c069af95697e667317dbdcb7e65ec9444
Ognezbozhev <valeriy.polkovnikov@gmail.com>
2023-07-12 12:22:26 +0400

    HT4

---
+++ HT4/HT4e.py
@@ -0,0 +1,8 @@
+k = int(input ())
+x = int(input ())
+b = int(input ())
+y = int(input ())
+
+if y==k*x+b:
+  print ('all your base belong to us')
+else: print ('all your base are not belong to us')
