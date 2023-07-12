00fbd064c2d2c6a8270da8f6552f4a6147e02a8a
Ognezbozhev <valeriy.polkovnikov@gmail.com>
2023-07-12 12:10:05 +0400

    HT 3 edited

--- HT3/HT3.py
+++ HT3/HT3.py
@@ -1,12 +1,11 @@
 import time
 import calendar
-import datetime
-import datetime as dt
 a=int(time.time())
-b=int((time.time())/86400)
+sec = 86400
+b=int((time.time())/sec)
 c=int(b/365)
 d=int(c/4)
-e=int(((a-((((365*40)*24)*60)*60)-((((366*13)*24)*60)*60))/86400)+1)
+e=int(((a-((365*40)*sec)-((366*13)*sec))/sec)+1)
 print (a, 'current timestamp')
 print (b, 'days from Unix start')
 print (c, 'years from Unix start')
@@ -16,16 +15,22 @@ print (e, 'th day from the start of this year')
 print ('-------------------------------------------------------------------------------------')
 
 from datetime import date
+from datetime import datetime
+
+now = datetime.now()
+year = now.year
+month = now.month
+day = now.day
 
 d0 = date(1970, 1, 1)
-d1 = date(2023, 7, 9)
+d1 = date(year, month, day)
 delta = d1 - d0
 print(delta.days ,'days from Unix start chyotenko edition')
 
 print (int(delta.days/365) ,'years from Unix start chyotenko edition')
 
 d0 = date(2023, 1, 1)
-d1 = date(2023, 7, 9)
+d1 = date(year, month, day)
 delta = d1 - d0
 print(delta.days +1 ,'days from year start chyotenko edition')
 
